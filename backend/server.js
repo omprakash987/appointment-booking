64be45e70c7970d94aa4afd94e2aa1d395356c4a
omprakash parewa <kanuparewa371@gmail.com>
2025-01-11 18:01:49 +0530

    add build script

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,16 @@ import availableRoutes from './routes/available.route.js'
 import appointmentRoutes from './routes/appointment.route.js'
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import path from 'path'; 
 
 dotenv.config(); 
 const corsOptions = {
     origin: 'http://localhost:5173', 
     credentials: true, 
 };
+
+const PORT = 8000||process.env.PORT
+const __dirname = path.resolve(); 
 
 const app  = express(); 
 app.use(cors(corsOptions))
@@ -25,7 +29,14 @@ app.use('/api/auth',authRouters);
 app.use('/api',availableRoutes); 
 app.use('/api',appointmentRoutes); 
 
-app.listen(8000,()=>{
-    console.log("port is running on 8000")
-    connectToDB(); 
+if(process.env.NODE === 'production'){
+    app.use(express.static(path.join(__dirname,"/frontend/dist"))); 
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,'frontend',"dist","index.html"))
+})
+}
+
+app.listen(PORT,()=>{
+    console.log(`port is running on ${PORT}`)
+    connectToDB();
 })
